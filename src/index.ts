fe7174b90bda7f674a634a73b1ee8d2482e8712e
wangjh <vong.giunfa@gmail.com>
2025-02-28 09:29:31 +0800

    feat：commit

--- src/index.ts
+++ src/index.ts
@@ -201,21 +201,6 @@ app.get('/', (req, res) => {
   res.send('欢迎访问 Express 服务器！');
 });
 
-// 用户路由
-app.get('/users', (req, res) => {
-  res.json([
-    { id: 1, name: '张三' },
-    { id: 2, name: '李四' },
-    { id: 3, name: '王五' }
-  ]);
-});
-
-// 获取单个用户信息
-app.get('/users/:id', (req, res) => {
-  const userId = req.params.id;
-  res.json({ id: userId, name: `用户${userId}` });
-});
-
 // 启动服务器
 app.listen(port, () => {
   console.log(`服务器运行在 http://localhost:${port}`);
